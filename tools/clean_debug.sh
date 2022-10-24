0735494cbd34765d85409bbac69af33dfd228d57
Matteo Winter <matwinte.42@bluewin.ch>
2022-10-24 20:50:28 +0200

    cleanup

--- tools/clean_debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-echo "Remove the following files ?"
-echo "----------------------------"
-find . -name "*.out" -print
-echo "----------------------------"
-echo "Enter to delete or ctrl-c to exit"
-read
-find . -name "*.out" -print -delete
-
