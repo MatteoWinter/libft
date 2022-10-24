7ceeda1034e4fbda9b9f334114870feb11724a69
Matteo Winter <matwinte.42@bluewin.ch>
2022-10-24 23:41:19 +0200

    memmove more efficient and less posix compliant

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: matwinte <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 21:48:35 by matwinte          #+#    #+#             */
-/*   Updated: 2022/10/24 21:58:54 by matwinte         ###   ########.fr       */
+/*   Updated: 2022/10/24 22:37:00 by matwinte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	void *tmp;
+	char	*s;
+	char	*d;
 
-	tmp = malloc(sizeof(char) * n);
-	ft_memcpy(tmp, src, n);
-	ft_memcpy(dest, tmp, n);
-	free(tmp);
+	s = (char *) src;
+	d = (char *) dest;
+	if (src < dest)
+		while (n--)
+			d[n] = s[n];
+	if (dest < src)
+		while (n--)
+			*d++ = *s++;
 	return (dest);
 }
