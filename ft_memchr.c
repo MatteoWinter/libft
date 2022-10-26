a7a147964abe67ebe4d8363f84e2223e1b092686
Matteo Winter <matwinte.42@bluewin.ch>
2022-10-26 04:27:20 +0200

    memchr done & tested

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: matwinte <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 02:42:53 by matwinte          #+#    #+#             */
-/*   Updated: 2022/10/26 02:52:19 by matwinte         ###   ########.fr       */
+/*   Updated: 2022/10/26 04:26:49 by matwinte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
+	unsigned char	*mem;
 
+	mem = (unsigned char *) s;
+	while (n--)
+	{
+		if (*mem == (unsigned char) c)
+			return (mem);
+		mem++;
+	}
+	return (NULL);
 }
