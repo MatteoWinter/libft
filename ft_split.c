7c663176bdf2cfc1138680539482b49ab92cad69
Matteo Winter <matwinte.42@bluewin.ch>
2022-10-27 22:51:51 +0200

    ft_split test ok !

--- ft_split.c
+++ ft_split.c
@@ -5,93 +5,75 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: matwinte <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/27 12:10:17 by matwinte          #+#    #+#             */
-/*   Updated: 2022/10/27 16:06:44 by matwinte         ###   ########.fr       */
+/*   Created: 2022/10/27 16:10:14 by matwinte          #+#    #+#             */
+/*   Updated: 2022/10/27 22:46:37 by matwinte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_getnextsep(char const *s, char c)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i] != c && s[i])
-		i++;
-	return (i);
-}
-
-size_t ft_getnextchar(char const *s, char c)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i] == c)
-		i++;
-	return (i);
-}
-
-size_t	ft_wordcount(char const *s, char c)
-{
-	size_t	i;
-	size_t	w;
-
-	i = 0;
-	w = 0;
-	while (s[i])
-	{
-		i += ft_getnextsep(&s[i], c);
-		if (s[i] == c)
-			i+= ft_getnextchar(&s[i], c);
-		w++;
-	}
-	return (w);
-}
-
 char	**ft_split(char const *s, char c)
 {
 	size_t	i;
+	size_t	j;
 	size_t	w;
-	size_t	start;
-	size_t	len;
 	char	**p;
 
+	// edge cases
+	if (s == NULL || c == '\0' || *s == '\0')
+	{
+		p = malloc(sizeof(char **));
+		p[0] = NULL;
+		return (p);
+	}
+
+	// count words
 	i = 0;
+	j = 0;
 	w = 0;
-	start = 0;
-	if (s == NULL)
-		return (NULL);
-	w = ft_wordcount(s, c);
-	if (w == 0)
-		return (NULL);
-	p = malloc(sizeof(char *) * w + 1);
+	while (s[i++])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] == '\0')
+			break ;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		w++;
+	}
+
+	p = malloc(sizeof(char *) * (w + 1));
 	if (p == NULL)
 		return (NULL);
 	i = 0;
-	while (w-- && s[start])
+
+	// fill array
+	while (w--)
 	{
-		start += ft_getnextchar(&s[start], c);
-		if (s[start] == '\0')
-			break ;
-		len = ft_getnextsep(&s[start], c);
-		p[i++] = ft_substr(s, start, len);
-		start += len;
+		while (*s == c)
+			s++;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		p[j++] = ft_substr(s, 0, i);
+		s += i;
+		i = 0;
 	}
-	p[i] = NULL;
+	p[j] = NULL;
 	return (p);
 }
 
-/*
+#if 0
 #include <stdio.h>
 
 int main(void)
 {
 	int i = 0;
-	char **strs = ft_split("_,f,,a,1aaa,2aaa,3aaaa,a aasd,a4,5,6,,,,,ff", ',');
+	char **strs = ft_split("      split       this for   me  !       ", ' ');
 
 	while (strs[i])
 		printf("ret: \"%s\"\n", strs[i++]);
+	if (strs[i] == NULL)
+		printf("ret: NULL\n");
 	return (0);
 }
-*/
+#endif
