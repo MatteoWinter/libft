ccdd3967ef7bd2652de306a3ef17e1371b0a1915
Matteo Winter <matwinte.42@bluewin.ch>
2022-10-27 11:17:27 +0200

    back to old version

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: matwinte <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 23:43:53 by matwinte          #+#    #+#             */
-/*   Updated: 2022/10/26 23:19:42 by matwinte         ###   ########.fr       */
+/*   Updated: 2022/10/27 11:15:10 by matwinte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	dst_len;
 
 	i = 0;
-	if (size == 0)
-		return (0);
 	dst_len = ft_strlen(dst);
 	if (size <= dst_len)
 		return (ft_strlen(src) + size);
