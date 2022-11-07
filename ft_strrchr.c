1e216d16e2a0b0163604b6812151a31eed40e0bc
Matteo Winter <matwinte.42@bluewin.ch>
2022-11-07 21:28:27 +0100

    fcn rewrite

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: matwinte <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 00:47:44 by matwinte          #+#    #+#             */
-/*   Updated: 2022/10/26 01:04:40 by matwinte         ###   ########.fr       */
+/*   Updated: 2022/11/07 21:28:11 by matwinte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ char	*ft_strrchr(const char *s, int c)
 {
 	long int	i;
 
-	i = ft_strlen(s);
-	while (i > -1 && s[i] != (char) c)
-		i--;
-	if (s[i] == (char) c)
-		return ((char *) &s[i]);
+	i = ft_strlen(s) + 1;
+	while (--i > -1)
+		if (s[i] == (unsigned char) c)
+			return ((char *) s + i);
 	return (NULL);
 }
